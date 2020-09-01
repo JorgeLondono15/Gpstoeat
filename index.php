a4919ad134ecfe4056dc961cc7602a0a346bfe68
jamesmejia <jamesmejia-1996@hotmail.com>
2020-09-01 18:29:09 -0500

    listas y tablas

--- index.php
+++ index.php
@@ -20,5 +20,105 @@
         <h2>¿Como será al final del semestre?</h2>
         <br> Buscamos integrar al cliente a nuestra pagina, con unos apartados muy sencillos pero elegantes, que permitan una facil movilidad de los usuarios.    
     </p>
+    <h1>ejercicios con tablas</h1>
+    <table style="width: 100%;" >
+<tr> 
+    <th>
+        nombre
+    </th>
+    <th>
+        apellido
+    </th>
+    <th>
+       cedula
+    </th>
+    </tr>
+<td>Luisa</td>
+<td>aguirre</td>
+<td>1241545</td>
+</tr>
+<tr> 
+<td>jorge</td>
+<td>londoño</td>
+<td>154554478</td>
+</tr>
+<tr> 
+<td>james</td>
+<td>mejia</td>
+<td>101214554</td>
+</tr>
+
+    </table>
+
+    <ol type="I">
+    <li>elemento 1</li>
+    <li>elemento 2</li>
+    <li>elemento 3</li>
+    </ol>
+
+    <ol style="list-style-type:disc;">
+    <li>elemento 1</li>
+    <li>elemento 2</li>
+    <li>elemento 3</li>
+    </ol>
+    
+    <ol style="list-style-type:circle;">
+    <li>elemento 1</li>
+    <li>elemento 2</li>
+    <li>elemento 3</li>
+    </ol>
+    
+    <ol style="list-style-type:square;">
+    <li>elemento 1</li>
+    <li>elemento 2</li>
+    <li>elemento 3</li>
+    </ol>
+    
+    <ol style="list-style-type:nome;">
+    <li>elemento 1</li>
+    <li>elemento 2</li>
+    <li>elemento 3</li>
+    </ol>
+    
+    <ol style="list-type-type:disc;">
+    <li> selección 1</li>
+      <ol style="type-style-type:circle;"> 
+     <li>elemento 1</li>
+     <li>elemento 2</li>
+     <li>elemento 3</li>
+    </ol>
+    <li> selección 1</li>
+
+    </ol>
+    <br>
+
+
+     <form action="otra.html" method="get">
+    <label>nombre:</label>
+    <input type="text"> name="nombre">
+</br>
+
+    <label>cedula:</label>
+    <input type="text"> name="cedula">
+</br>
+
+    <label>contraseña:</label>
+    <input type="text"> name="contraseña">
+</br>
+
+    <label>telefono:</label>
+    <input type="text"> name="telefono">
+</br>
+
+    <button type="submit">Enviar</button>
+
+
+
+</form>
+
+ <center><iframe width="560" height="315" src="https://libroayudate.files.wordpress.com/2015/07/walter-riso-enamorate-de-ti.pdf"></iframe></center> 
+
+ <br>
+ <center><iframe width="560" height="315" src="https://www.youtube.com/embed/Bj4tTjBbIzw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
 </body>
 </html>
