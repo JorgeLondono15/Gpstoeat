fcb185448f89dbbe6e57cf4d90f97c994cabe5f7
JorgeLondono15 <Jorge.londono@corre.tdea.edu.co>
2020-09-25 20:00:22 -0500

    Nuevos estilos, organizando la pagina y el aside

--- index.php
+++ index.php
@@ -11,17 +11,35 @@
 
     <title>Prueba Bootstrap</title>
   </head>
+
   <body class=margenes>
+
   <DIV STYLE="position:absolute; top:20px; left:20px; visibility:visible z-index:1">
 <IMG height="10%" SRC="imagenes/Logo-2.png" width="10%">
 </div>
+
 <div class=mapa>
 <iframe src="https://www.google.com/maps/d/embed?mid=1BsuxtKk8vtLtg8MmCe2AnpcyPjZjTjLJ" width="850" height="600"></iframe>
 </div>
 
 <aside style="float: right;  margin-top: 15px; width: 32%; height: 600px; background-color: mistyrose; margin-right:20px; border : 1px solid black">
-			<p>Ejemplo de una sección ubicada al lado de otra debido a que el elemento anterior ocupaba un 60% de la pantalla, y el aside ocupa un 30%. Está ubicado a la derecha de la linea y tiene un margen de 5% con respecto al borde. Ocupa el 50% del alto de la página según el tamaño de la pantalla donde se visualice</p>	
-		</aside>
+
+<form action="formularios-select-1.php" method="get">
+<fieldset>
+  <legend><h1>Tipo de comida</h1></legend>
+<select name="menu">
+  <option value="0" selected>Elegir</option>
+  <option value="1" selected>Quesos</option>
+  <option value="2" selected>Pizza</option>
+  <option value="3" selected>Arepas</option>
+  <option value="4" selected>Palitos de Queso</option>
+  <option value="5" selected>Papas Fritas</option>
+  <option value="6" selected>Mandingas</option>
+</select>
+</fieldset>
+</form>  
+
+</aside>
 
 
 
